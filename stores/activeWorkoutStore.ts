988f58cfdc087c868cd1aef7abbad58e7871c7fb
Nils Fischer <nils-fischer7@web.de>
2024-12-19 21:19:10 +0100

    improve activeWorkoutStore

--- stores/activeWorkoutStore.ts
+++ stores/activeWorkoutStore.ts
@@ -1,31 +1,26 @@
 import { create } from "zustand";
-import type { ExerciseRecord, SetInput, WorkoutExercise, WorkoutSession } from "~/lib/types";
+import type { SetInput, Workout, WorkoutSession } from "~/lib/types";
 
 interface ActiveWorkoutState {
-  // Workout Status
   isStarted: boolean;
   isPaused: boolean;
   startTime: number | null;
   elapsedTime: number;
 
-  // Workout Data
-  currentWorkout: {
-    exercises: ExerciseRecord[];
-    workoutId?: number;
-  } | null;
+  currentWorkout: Workout | null;
+  currentSession: WorkoutSession | null;
 
-  // Timer Interval
   timerInterval: NodeJS.Timeout | null;
 
-  // Actions
-  startWorkout: (workoutId: number | undefined, exercises: WorkoutExercise[]) => void;
+  setWorkout: (workout: Workout) => void;
+  startWorkout: () => void;
   pauseWorkout: () => void;
   resumeWorkout: () => void;
-  endWorkout: () => WorkoutSession | null;
+  cancelWorkout: () => void;
+  finishWorkout: () => WorkoutSession | null;
 
-  // Exercise Progress
-  updateExerciseRecord: (exerciseId: number, sets: SetInput[]) => void;
-  getExerciseRecord: (exerciseId: number) => ExerciseRecord | null;
+  addSet: (exerciseId: number, newSet: SetInput) => void;
+  finishExercise: (exerciseId: number, intensity?: number) => void;
 
   // Stats
   getCurrentStats: () => {
@@ -33,8 +28,9 @@ interface ActiveWorkoutState {
     totalVolume: number;
     completedExercises: number;
     remainingExercises: number;
+    completedSets: number;
+    remainingSets: number;
   };
-  getCompletedSets: (exerciseId: number) => SetInput[] | undefined;
 }
 
 export const useActiveWorkoutStore = create<ActiveWorkoutState>((set, get) => ({
@@ -44,21 +40,32 @@ export const useActiveWorkoutStore = create<ActiveWorkoutState>((set, get) => ({
   elapsedTime: 0,
   currentWorkout: null,
   timerInterval: null,
+  currentSession: null,
 
-  startWorkout: (workoutId, exercises) => {
-    // Transform WorkoutExercises into ExerciseRecords
-    const exerciseRecords: ExerciseRecord[] = exercises.map((exercise) => ({
-      exerciseId: exercise.exerciseId,
-      sets: [],
-      intensity: 0,
-      isCompleted: false,
-    }));
+  setWorkout: (workout: Workout) => {
+    set({ currentWorkout: workout });
+  },
 
-    // Clear any existing interval
+  startWorkout: () => {
+    const workout = get().currentWorkout;
+    if (!workout) {
+      console.error("No workout set");
+      return;
+    }
+    const session: WorkoutSession = {
+      date: new Date(),
+      entries: workout.exercises.map((exercise) => ({
+        exerciseId: exercise.exerciseId,
+        sets: [],
+        intensity: undefined,
+        isCompleted: false,
+      })),
+      workoutId: workout.id,
+    };
+
     const timerInterval = get().timerInterval;
     timerInterval && clearInterval(timerInterval);
 
-    // Start new timer
     const interval = setInterval(() => {
       set((state) => ({
         elapsedTime: state.startTime ? Date.now() - state.startTime : 0,
@@ -70,10 +77,7 @@ export const useActiveWorkoutStore = create<ActiveWorkoutState>((set, get) => ({
       isPaused: false,
       startTime: Date.now(),
       elapsedTime: 0,
-      currentWorkout: {
-        exercises: exerciseRecords,
-        workoutId,
-      },
+      currentSession: session,
       timerInterval: interval,
     });
   },
@@ -93,18 +97,27 @@ export const useActiveWorkoutStore = create<ActiveWorkoutState>((set, get) => ({
     set({ isPaused: false, timerInterval: interval });
   },
 
-  endWorkout: () => {
+  cancelWorkout: () => {
+    const timerInterval = get().timerInterval;
+    timerInterval && clearInterval(timerInterval);
+    set({
+      isStarted: false,
+      isPaused: false,
+      startTime: null,
+      elapsedTime: 0,
+      currentSession: null,
+      timerInterval: null,
+    });
+  },
+
+  finishWorkout: () => {
     const timerInterval = get().timerInterval;
     timerInterval && clearInterval(timerInterval);
 
     const currentWorkout = get().currentWorkout;
     if (!currentWorkout) return null;
 
-    const workoutSession: WorkoutSession = {
-      date: new Date(),
-      entries: currentWorkout.exercises,
-      workoutId: currentWorkout.workoutId,
-    };
+    const currentSession = get().currentSession;
 
     set({
       isStarted: false,
@@ -113,52 +126,66 @@ export const useActiveWorkoutStore = create<ActiveWorkoutState>((set, get) => ({
       elapsedTime: 0,
       currentWorkout: null,
       timerInterval: null,
+      currentSession: null,
     });
 
-    return workoutSession;
+    return currentSession;
   },
 
-  updateExerciseRecord: (exerciseId, sets) => {
+  addSet: (exerciseId: number, newSet: SetInput) => {
     set((state) => ({
-      currentWorkout: state.currentWorkout
+      currentSession: state.currentSession
         ? {
-            ...state.currentWorkout,
-            exercises: state.currentWorkout.exercises.map((exercise) =>
-              exercise.exerciseId === exerciseId ? { ...exercise, sets, isCompleted: true } : exercise
+            ...state.currentSession,
+            entries: state.currentSession.entries.map((entry) =>
+              entry.exerciseId === exerciseId ? { ...entry, sets: [...entry.sets, newSet] } : entry
             ),
           }
         : null,
     }));
   },
 
-  getExerciseRecord: (exerciseId) => {
-    return get().currentWorkout?.exercises.find((exercise) => exercise.exerciseId === exerciseId) || null;
+  finishExercise: (exerciseId: number, intensity = undefined) => {
+    set((state) => ({
+      currentWorkout: state.currentWorkout
+        ? {
+            ...state.currentWorkout,
+            exercises: state.currentWorkout.exercises.map((exercise) =>
+              exercise.exerciseId === exerciseId ? { ...exercise, isCompleted: true, intensity } : exercise
+            ),
+          }
+        : null,
+    }));
   },
 
   getCurrentStats: () => {
-    const currentWorkout = get().currentWorkout;
-    if (!currentWorkout) {
+    const currentSession = get().currentSession;
+    if (!currentSession) {
       return {
         duration: 0,
         totalVolume: 0,
         completedExercises: 0,
         remainingExercises: 0,
+        completedSets: 0,
+        remainingSets: 0,
       };
     }
 
-    const totalVolume = currentWorkout.exercises.reduce((acc, exercise) => {
-      return acc + exercise.sets.reduce((setAcc, set) => setAcc + (set.weight || 0) * (set.reps || 0), 0);
+    const totalVolume = currentSession.entries.reduce((acc, entry) => {
+      return acc + entry.sets.reduce((setAcc, set) => setAcc + (set.weight || 0) * (set.reps || 0), 0);
     }, 0);
+
+    const completedSets = currentSession.entries.reduce((acc, entry) => acc + entry.sets.length, 0);
+
+    const totalTargetSets = currentSession.entries.reduce((acc, entry) => acc + (entry.sets?.length || 0), 0);
 
     return {
       duration: get().elapsedTime,
       totalVolume,
-      completedExercises: currentWorkout.exercises.filter((e) => e.isCompleted).length,
-      remainingExercises: currentWorkout.exercises.filter((e) => !e.isCompleted).length,
+      completedExercises: currentSession.entries.filter((e) => e.isCompleted).length,
+      remainingExercises: currentSession.entries.filter((e) => !e.isCompleted).length,
+      completedSets,
+      remainingSets: totalTargetSets - completedSets,
     };
-  },
-  getCompletedSets: (exerciseId: number) => {
-    const exerciseRecord = get().currentWorkout?.exercises.find((exercise) => exercise.exerciseId === exerciseId);
-    return exerciseRecord?.sets;
   },
 }));
